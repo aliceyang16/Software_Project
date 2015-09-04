5c25409ec3a6d85cd2af8594ca0324b0a217e115
aliceyang16 <yangalice8@gmail.com>
2015-09-04 10:37:46 +0200

    initial

---
+++ Debug/main.cpp.o.d
@@ -0,0 +1,253 @@
+Debug/main.cpp.o: main.cpp C:/sfml/include/SFML/Graphics.hpp \
+ C:/sfml/include/SFML/Window.hpp C:/sfml/include/SFML/System.hpp \
+ C:/sfml/include/SFML/Config.hpp C:/sfml/include/SFML/System/Clock.hpp \
+ C:/sfml/include/SFML/System/Export.hpp \
+ C:/sfml/include/SFML/System/Time.hpp C:/sfml/include/SFML/System/Err.hpp \
+ C:/sfml/include/SFML/System/FileInputStream.hpp \
+ C:/sfml/include/SFML/System/InputStream.hpp \
+ C:/sfml/include/SFML/System/NonCopyable.hpp \
+ C:/sfml/include/SFML/System/Lock.hpp \
+ C:/sfml/include/SFML/System/MemoryInputStream.hpp \
+ C:/sfml/include/SFML/System/Mutex.hpp \
+ C:/sfml/include/SFML/System/Sleep.hpp \
+ C:/sfml/include/SFML/System/String.hpp \
+ C:/sfml/include/SFML/System/Utf.hpp C:/sfml/include/SFML/System/Utf.inl \
+ C:/sfml/include/SFML/System/String.inl \
+ C:/sfml/include/SFML/System/Thread.hpp \
+ C:/sfml/include/SFML/System/Thread.inl \
+ C:/sfml/include/SFML/System/ThreadLocal.hpp \
+ C:/sfml/include/SFML/System/ThreadLocalPtr.hpp \
+ C:/sfml/include/SFML/System/ThreadLocalPtr.inl \
+ C:/sfml/include/SFML/System/Vector2.hpp \
+ C:/sfml/include/SFML/System/Vector2.inl \
+ C:/sfml/include/SFML/System/Vector3.hpp \
+ C:/sfml/include/SFML/System/Vector3.inl \
+ C:/sfml/include/SFML/Window/Context.hpp \
+ C:/sfml/include/SFML/Window/Export.hpp \
+ C:/sfml/include/SFML/Window/GlResource.hpp \
+ C:/sfml/include/SFML/Window/ContextSettings.hpp \
+ C:/sfml/include/SFML/Window/Event.hpp \
+ C:/sfml/include/SFML/Window/Joystick.hpp \
+ C:/sfml/include/SFML/Window/Keyboard.hpp \
+ C:/sfml/include/SFML/Window/Mouse.hpp \
+ C:/sfml/include/SFML/Window/Sensor.hpp \
+ C:/sfml/include/SFML/Window/Touch.hpp \
+ C:/sfml/include/SFML/Window/VideoMode.hpp \
+ C:/sfml/include/SFML/Window/Window.hpp \
+ C:/sfml/include/SFML/Window/WindowHandle.hpp \
+ C:/sfml/include/SFML/Window/WindowStyle.hpp \
+ C:/sfml/include/SFML/Graphics/BlendMode.hpp \
+ C:/sfml/include/SFML/Graphics/Export.hpp \
+ C:/sfml/include/SFML/Graphics/CircleShape.hpp \
+ C:/sfml/include/SFML/Graphics/Shape.hpp \
+ C:/sfml/include/SFML/Graphics/Drawable.hpp \
+ C:/sfml/include/SFML/Graphics/RenderStates.hpp \
+ C:/sfml/include/SFML/Graphics/Transform.hpp \
+ C:/sfml/include/SFML/Graphics/Rect.hpp \
+ C:/sfml/include/SFML/Graphics/Rect.inl \
+ C:/sfml/include/SFML/Graphics/Transformable.hpp \
+ C:/sfml/include/SFML/Graphics/VertexArray.hpp \
+ C:/sfml/include/SFML/Graphics/Vertex.hpp \
+ C:/sfml/include/SFML/Graphics/Color.hpp \
+ C:/sfml/include/SFML/Graphics/PrimitiveType.hpp \
+ C:/sfml/include/SFML/Graphics/ConvexShape.hpp \
+ C:/sfml/include/SFML/Graphics/Font.hpp \
+ C:/sfml/include/SFML/Graphics/Glyph.hpp \
+ C:/sfml/include/SFML/Graphics/Texture.hpp \
+ C:/sfml/include/SFML/Graphics/Image.hpp \
+ C:/sfml/include/SFML/Graphics/RectangleShape.hpp \
+ C:/sfml/include/SFML/Graphics/RenderTarget.hpp \
+ C:/sfml/include/SFML/Graphics/View.hpp \
+ C:/sfml/include/SFML/Graphics/RenderTexture.hpp \
+ C:/sfml/include/SFML/Graphics/RenderWindow.hpp \
+ C:/sfml/include/SFML/Graphics/Shader.hpp \
+ C:/sfml/include/SFML/Graphics/Sprite.hpp \
+ C:/sfml/include/SFML/Graphics/Text.hpp C:/sfml/include/SFML/Audio.hpp \
+ C:/sfml/include/SFML/Audio/InputSoundFile.hpp \
+ C:/sfml/include/SFML/Audio/Export.hpp \
+ C:/sfml/include/SFML/Audio/Listener.hpp \
+ C:/sfml/include/SFML/Audio/Music.hpp \
+ C:/sfml/include/SFML/Audio/SoundStream.hpp \
+ C:/sfml/include/SFML/Audio/SoundSource.hpp \
+ C:/sfml/include/SFML/Audio/AlResource.hpp \
+ C:/sfml/include/SFML/Audio/OutputSoundFile.hpp \
+ C:/sfml/include/SFML/Audio/Sound.hpp \
+ C:/sfml/include/SFML/Audio/SoundBuffer.hpp \
+ C:/sfml/include/SFML/Audio/SoundBufferRecorder.hpp \
+ C:/sfml/include/SFML/Audio/SoundRecorder.hpp \
+ C:/sfml/include/SFML/Audio/SoundFileFactory.hpp \
+ C:/sfml/include/SFML/Audio/SoundFileFactory.inl \
+ C:/sfml/include/SFML/Audio/SoundFileReader.hpp \
+ C:/sfml/include/SFML/Audio/SoundFileWriter.hpp
+
+C:/sfml/include/SFML/Graphics.hpp:
+
+C:/sfml/include/SFML/Window.hpp:
+
+C:/sfml/include/SFML/System.hpp:
+
+C:/sfml/include/SFML/Config.hpp:
+
+C:/sfml/include/SFML/System/Clock.hpp:
+
+C:/sfml/include/SFML/System/Export.hpp:
+
+C:/sfml/include/SFML/System/Time.hpp:
+
+C:/sfml/include/SFML/System/Err.hpp:
+
+C:/sfml/include/SFML/System/FileInputStream.hpp:
+
+C:/sfml/include/SFML/System/InputStream.hpp:
+
+C:/sfml/include/SFML/System/NonCopyable.hpp:
+
+C:/sfml/include/SFML/System/Lock.hpp:
+
+C:/sfml/include/SFML/System/MemoryInputStream.hpp:
+
+C:/sfml/include/SFML/System/Mutex.hpp:
+
+C:/sfml/include/SFML/System/Sleep.hpp:
+
+C:/sfml/include/SFML/System/String.hpp:
+
+C:/sfml/include/SFML/System/Utf.hpp:
+
+C:/sfml/include/SFML/System/Utf.inl:
+
+C:/sfml/include/SFML/System/String.inl:
+
+C:/sfml/include/SFML/System/Thread.hpp:
+
+C:/sfml/include/SFML/System/Thread.inl:
+
+C:/sfml/include/SFML/System/ThreadLocal.hpp:
+
+C:/sfml/include/SFML/System/ThreadLocalPtr.hpp:
+
+C:/sfml/include/SFML/System/ThreadLocalPtr.inl:
+
+C:/sfml/include/SFML/System/Vector2.hpp:
+
+C:/sfml/include/SFML/System/Vector2.inl:
+
+C:/sfml/include/SFML/System/Vector3.hpp:
+
+C:/sfml/include/SFML/System/Vector3.inl:
+
+C:/sfml/include/SFML/Window/Context.hpp:
+
+C:/sfml/include/SFML/Window/Export.hpp:
+
+C:/sfml/include/SFML/Window/GlResource.hpp:
+
+C:/sfml/include/SFML/Window/ContextSettings.hpp:
+
+C:/sfml/include/SFML/Window/Event.hpp:
+
+C:/sfml/include/SFML/Window/Joystick.hpp:
+
+C:/sfml/include/SFML/Window/Keyboard.hpp:
+
+C:/sfml/include/SFML/Window/Mouse.hpp:
+
+C:/sfml/include/SFML/Window/Sensor.hpp:
+
+C:/sfml/include/SFML/Window/Touch.hpp:
+
+C:/sfml/include/SFML/Window/VideoMode.hpp:
+
+C:/sfml/include/SFML/Window/Window.hpp:
+
+C:/sfml/include/SFML/Window/WindowHandle.hpp:
+
+C:/sfml/include/SFML/Window/WindowStyle.hpp:
+
+C:/sfml/include/SFML/Graphics/BlendMode.hpp:
+
+C:/sfml/include/SFML/Graphics/Export.hpp:
+
+C:/sfml/include/SFML/Graphics/CircleShape.hpp:
+
+C:/sfml/include/SFML/Graphics/Shape.hpp:
+
+C:/sfml/include/SFML/Graphics/Drawable.hpp:
+
+C:/sfml/include/SFML/Graphics/RenderStates.hpp:
+
+C:/sfml/include/SFML/Graphics/Transform.hpp:
+
+C:/sfml/include/SFML/Graphics/Rect.hpp:
+
+C:/sfml/include/SFML/Graphics/Rect.inl:
+
+C:/sfml/include/SFML/Graphics/Transformable.hpp:
+
+C:/sfml/include/SFML/Graphics/VertexArray.hpp:
+
+C:/sfml/include/SFML/Graphics/Vertex.hpp:
+
+C:/sfml/include/SFML/Graphics/Color.hpp:
+
+C:/sfml/include/SFML/Graphics/PrimitiveType.hpp:
+
+C:/sfml/include/SFML/Graphics/ConvexShape.hpp:
+
+C:/sfml/include/SFML/Graphics/Font.hpp:
+
+C:/sfml/include/SFML/Graphics/Glyph.hpp:
+
+C:/sfml/include/SFML/Graphics/Texture.hpp:
+
+C:/sfml/include/SFML/Graphics/Image.hpp:
+
+C:/sfml/include/SFML/Graphics/RectangleShape.hpp:
+
+C:/sfml/include/SFML/Graphics/RenderTarget.hpp:
+
+C:/sfml/include/SFML/Graphics/View.hpp:
+
+C:/sfml/include/SFML/Graphics/RenderTexture.hpp:
+
+C:/sfml/include/SFML/Graphics/RenderWindow.hpp:
+
+C:/sfml/include/SFML/Graphics/Shader.hpp:
+
+C:/sfml/include/SFML/Graphics/Sprite.hpp:
+
+C:/sfml/include/SFML/Graphics/Text.hpp:
+
+C:/sfml/include/SFML/Audio.hpp:
+
+C:/sfml/include/SFML/Audio/InputSoundFile.hpp:
+
+C:/sfml/include/SFML/Audio/Export.hpp:
+
+C:/sfml/include/SFML/Audio/Listener.hpp:
+
+C:/sfml/include/SFML/Audio/Music.hpp:
+
+C:/sfml/include/SFML/Audio/SoundStream.hpp:
+
+C:/sfml/include/SFML/Audio/SoundSource.hpp:
+
+C:/sfml/include/SFML/Audio/AlResource.hpp:
+
+C:/sfml/include/SFML/Audio/OutputSoundFile.hpp:
+
+C:/sfml/include/SFML/Audio/Sound.hpp:
+
+C:/sfml/include/SFML/Audio/SoundBuffer.hpp:
+
+C:/sfml/include/SFML/Audio/SoundBufferRecorder.hpp:
+
+C:/sfml/include/SFML/Audio/SoundRecorder.hpp:
+
+C:/sfml/include/SFML/Audio/SoundFileFactory.hpp:
+
+C:/sfml/include/SFML/Audio/SoundFileFactory.inl:
+
+C:/sfml/include/SFML/Audio/SoundFileReader.hpp:
+
+C:/sfml/include/SFML/Audio/SoundFileWriter.hpp:
---
+++ main.cpp
@@ -0,0 +1,160 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+
+/// \return Application exit code
+
+int main()
+{
+	std::srand(static_cast<unsigned int>(std::time(NULL)));
+	
+	 // Define some constants
+    const int gameWidth = 800;
+    const int gameHeight = 600;
+	sf::Vector2f playerSize(50, 50);
+	sf::Vector2f shooterSize(20,20);
+	sf::Vector2f bulletScale(0.05,0.05);
+	
+	// Create the window of the application
+    sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight, 32), "Shooter Game",
+                            sf::Style::Titlebar | sf::Style::Close);
+    window.setVerticalSyncEnabled(true);
+	
+	// Load the sounds used in the game
+    sf::SoundBuffer shootSoundBuffer;
+	if (!shootSoundBuffer.loadFromFile("resources/pewpew.wav"))
+        return EXIT_FAILURE;
+    sf::Sound shootSound(shootSoundBuffer);
+	
+	/**Load the image of the palyer*/
+	sf::Texture playerImage;
+	if (!playerImage.loadFromFile("resources/minion.png"))
+		return EXIT_FAILURE;
+		
+	/** Create the player*/
+	sf::Sprite player;
+	player.setTexture(playerImage);
+    player.setOrigin(playerSize / 2.f);
+	
+	/** Load the image of the shooter*/
+	sf::Texture shooterImage;
+	if (!shooterImage.loadFromFile("resources/banana.png"))
+		return EXIT_FAILURE;
+	
+	sf::Sprite bullet;
+	bullet.setTexture(shooterImage);
+	bullet.scale(bulletScale);
+	//bullet.setOrigin(shooterSize/ 2.f);
+	
+	/** Load the text font*/
+    sf::Font font;
+    if (!font.loadFromFile("resources/sansation.ttf"))
+        return EXIT_FAILURE;
+
+    // Initialize the pause message
+    sf::Text pauseMessage;
+    pauseMessage.setFont(font);
+    pauseMessage.setCharacterSize(40);
+    pauseMessage.setPosition(170.f, 150.f);
+    pauseMessage.setColor(sf::Color::White);
+    pauseMessage.setString("Welcome to Shooter!\nPress space to start the game");
+	
+	// Define the paddles properties
+	sf::Clock AITimer;
+	const sf::Time AITime   = sf::seconds(0.1f);
+	const float playerSpeed = 400.f;
+	const float bulletSpeed = 400.f;
+	float bulletAngle = 0.f;
+	
+	sf::Clock clock;
+	bool isPlaying = false;
+	while(window.isOpen())
+	{
+		// Handle events
+        sf::Event event;
+        while (window.pollEvent(event))
+		{
+			// Window closed or escape key pressed: exit
+            if ((event.type == sf::Event::Closed) ||
+               ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)))
+            {
+                window.close();
+                break;
+            }
+			
+			// Space key pressed: play
+            if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Space))
+            {
+                if (!isPlaying)
+                {
+                    // (re)start the game
+                    isPlaying = true;
+                    clock.restart();
+
+                    // Reset the position of the player and bullet
+                    player.setPosition(gameWidth / 2, gameHeight / 2);
+                }
+            }
+		}
+		
+		
+		
+		if (isPlaying)
+		{
+			float deltaTime = clock.restart().asSeconds();
+			
+            // Move the player's paddle
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) &&
+               (player.getPosition().y - playerSize.y / 2 > 5.f))
+            {
+                player.move(0.f, -playerSpeed * deltaTime);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) &&
+               (player.getPosition().y + playerSize.y / 2 < gameHeight - 5.f))
+            {
+                player.move(0.f, playerSpeed * deltaTime);
+            }
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) &&
+			   (player.getPosition().y - playerSize.y / 2 > 2.f))
+			{
+				player.move(playerSpeed * deltaTime,0.f);
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) &&
+			   (player.getPosition().y + playerSize.y / 2 < gameWidth - 2.f))
+			{
+				   player.move(-playerSpeed * deltaTime,0.f);
+			}
+			
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+			{
+				
+				bullet.setPosition(player.getPosition().x, player.getPosition().y);
+				//bullet.setPosition(gameWidth / 2, gameHeight / 2);
+				float factor = bulletSpeed * deltaTime;
+				bullet.move(std::cos(bulletAngle) * factor, std::sin(bulletAngle) * factor);
+			}
+		}
+		
+		// Clear the window
+        window.clear(sf::Color(50,50,50));
+
+        if (isPlaying)
+        {
+            // Draw the paddles and the ball
+            window.draw(player);
+			window.draw(bullet);
+        }
+        else
+        {
+            // Draw the pause message
+            window.draw(pauseMessage);
+        }
+		
+		// Display things on screen
+        window.display();
+	}
+	
+	return EXIT_SUCCESS;
+}
